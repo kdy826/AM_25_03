28a32b068b51b2f72cbb6e0c900ed609ffffb151
kdy826 <kimdy66695@gmail.com>
2025-03-25 16:40:32 +0900

    회원가입, 검색기능,중복제거

---
+++ src/main/java/org/example/Util.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.time.LocalDateTime;  // LocalDateTime 클래스에게서 임포트 받음
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+    public static String getNowStr() {  //현재 날짜 및 시간을 가져오는 함수(str)
+        LocalDateTime now = LocalDateTime.now();  //지금시간 확인
+        String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));// 시간 재설정
+        return formatedNow;
+    }
+}
